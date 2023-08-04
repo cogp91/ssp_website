b4da1ef7dbac33a9925e371c84511f754380abee
Carlos Galvez <omargalvezp8@gmail.com>
2023-08-04 13:15:52 -0600

    origin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import styles from './style';
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
-const express = require("express");
-const app = express();
 
-// This line is important to ensure your app listens to the PORT env var
-const port = process.env.PORT ?? 8080;
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
